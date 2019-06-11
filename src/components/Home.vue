483dcc1a3e5c1121f3026ab61f8214768d529cbb
Caleb <caleb.ziegler@gmail.com>
2019-06-11 18:00:55 -0500

    Add clickable cards to home page and clean up home page.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,75 @@
 <template>
-    <v-container>
-        <v-layout row>
+    <v-container grid-list-md>
+        <v-layout row mb-4>
             <div class="title">Welcome to the surveys for New Horizons 2019!</div>
         </v-layout>
-        <v-layout row mt-3>
-            <div class="subheading">We are administering three surveys: the entry survey (for inprocessing personnel), the camp survey (to collect information during exercise execution), and the exit survey (for outprocessing personnel).</div>
-        </v-layout>
         <v-layout row>
-            <div class="subheading">You can find the surveys in the top right corner of the screen.</div>
+            <v-flex xs4>
+                <v-hover>
+                    <v-card slot-scope="{ hover }"
+                            :class="`elevation-${hover ? 12 : 2}`"
+                            style="cursor: pointer"
+                            @click="$router.push('entrySurvey')">
+                        <v-card-title primary-title>
+                            <div>
+                                <h3 class="headline mb-3">Entry Survey</h3>    
+                                <div class="subheading">
+                                    For in-processing personnel (or for those who haven't taken an entry survey yet) 
+                                </div>
+                            </div>
+                        </v-card-title>
+                        <v-card-actions>
+                            <v-btn flat 
+                                    color="primary" 
+                                    to="/entrySurvey" router>Begin</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-hover>
+            </v-flex>
+            <v-flex xs4>
+                <v-hover>
+                    <v-card slot-scope="{ hover }"
+                            :class="`elevation-${hover ? 12 : 2}`"
+                            style="cursor: pointer"
+                            @click="$router.push('campSurvey')">
+                        <v-card-title primary-title>
+                            <div>
+                                <h3 class="headline mb-3">Camp Survey</h3>    
+                                <div class="subheading">
+                                    Mid-term survey for advising the commander 
+                                </div>
+                            </div>
+                        </v-card-title>
+                        <v-card-actions>
+                            <v-btn flat 
+                                    color="primary" 
+                                    to="/campSurvey" router>Begin</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-hover>
+            </v-flex>
+            <v-flex xs4>
+                <v-hover>
+                    <v-card slot-scope="{ hover }"
+                            :class="`elevation-${hover ? 12 : 2}`"
+                            style="cursor: pointer"
+                            @click="$router.push('exitSurvey')">
+                        <v-card-title primary-title>
+                            <div>
+                                <h3 class="headline mb-3">Exit Survey</h3>    
+                                <div class="subheading">
+                                    For out-processing personnel 
+                                </div>
+                            </div>
+                        </v-card-title>
+                        <v-card-actions>
+                            <v-btn flat 
+                                    color="primary" 
+                                    to="/exitSurvey" router>Begin</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-hover>
+            </v-flex>
         </v-layout>
     </v-container>
 
